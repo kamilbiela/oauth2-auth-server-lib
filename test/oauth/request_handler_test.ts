4407d9a733490450fd10117987e7245afe238e45
Kamil Biela <kamil.biela@gmail.com>
2016-01-20 00:59:18 +0100

    --

--- test/oauth/request_handler_test.ts
+++ test/oauth/request_handler_test.ts
@@ -4,7 +4,7 @@ import ServerConfig from "../../src/oauth/server_config";
 import * as errors from "../../src/oauth/errors";
 
 describe('RequestHandler', () => {
-    describe('authorization request', () => {
+    describe('3.1 Authorization Endpoint', () => {
 
         let requestHandler: RequestHandler = null;
         let serverConfig = new ServerConfig();
@@ -13,19 +13,45 @@ describe('RequestHandler', () => {
             requestHandler = new RequestHandler(serverConfig);
         });
 
-        it('should throw exception on wrong response_type', () => {
-            let response;
-            try {
-                response = requestHandler.handleAuthorizationRequest(
-                    "code_not", "http://redirect", "client1"
-                );
-            } catch (e) {
-                return assert.ok(e instanceof errors.UnsupportedResponseTypeError, "Should throw UnsupportedResponseTypeError on wrong response type value");
-            }
+        describe('3.1.1 response_type value MUST be "code" or "token" or registered extension value', () => {
+            it('Should throw UnsupportedResponseTypeError when response_type is wrong', () => {
+                let response;
+                try {
+                    response = requestHandler.handleAuthorizationRequest(
+                        "code_not", "http://redirect", "client1"
+                    );
+                } catch (e) {
+                    return assert.ok(e instanceof errors.UnsupportedResponseTypeError, "Should throw UnsupportedResponseTypeError on wrong response type value");
+                }
 
-            assert.ok(response, "Response can't be null / undefined / false");
+                assert.ok(response, "Response can't be null / undefined / false");
 
-            assert.fail(null, null, "Should throw an exception");
+                assert.fail(null, null, "Should throw an exception");
+            });
+
+            it('Should not trow error when response_type is valid value', () => {
+                for (let responseType in ['code', 'token']) {
+                    let response;
+                    try {
+                        response = requestHandler.handleAuthorizationRequest(
+                            responseType, "http://redirect", "client1"
+                        );
+                    } catch (e) {
+                        return assert.fail(null, null, `Got error response ${e.message}`);
+                    }
+                }
+            });
+
+            it(`3.1.2.4 if redirection URI is missing, invalid, or mismatching, the authorization server
+                SHOULD inform the resource owner of the error and MUST NOT automatically redirect the user-agent to the invalid redirection URI`,
+                () => {
+
+                }
+            );
         });
+    });
+
+    describe('3.2 Token Endpoint', () => {
+
     });
 });
